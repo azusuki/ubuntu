9e8f93f8f2012494fb9f99bb5135652d5b760e2f
asu <asu@example.com>
2022-03-03 09:56:40 +0900

    CreateView登録画面まで動くまで

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,8 @@ from myapp.models import Area, Category
 from django_datatables_view.base_datatable_view import BaseDatatableView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib import messages
+from django.shortcuts import resolve_url
 
 def index(request):
     #areas = Area.objects.all()
@@ -26,7 +28,7 @@ class CreateView(CreateView):
     #操作するモデルを指示
     model = Category
     #成功した時のJUMP先を指示
-    success_url = reverse_lazy("myapp:category_list")
+    success_url = reverse_lazy("myapp:list")
     template_name = 'category_form.html'
 
     #フィール設定（ないとエラー）
@@ -34,4 +36,4 @@ class CreateView(CreateView):
     # 投稿に成功した時に実行される処理
     def get_success_url(self):
         messages.success(self.request, '記事を投稿しました。')
-        return resolve_url("myapp:category_list")
+        return resolve_url("myapp:list")
