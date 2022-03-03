115465bf49a67d19ee90291ad59ac6c5c1444ab0
asu <asu@example.com>
2022-03-03 09:32:20 +0900

    CreateView登録画面まで

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('create/', views.CreateView.as_view(), name='create'),
+    path('',views.CategoryList.as_view(), name='list'),
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from myapp.models import Area, Category
 from django_datatables_view.base_datatable_view import BaseDatatableView
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse_lazy
 
 def index(request):
     #areas = Area.objects.all()
@@ -15,8 +16,22 @@ def index(request):
 
 class CategoryList(ListView):
     #Categoryテーブル連携
-    model = models.Category
+    model = Category
     #レコード情報をテンプレートに渡すオブジェクト
     context_object_name = "category_list"
     #テンプレートファイル連携
-    template_name = "Category.html"
+    template_name = "category.html"
+
+class CreateView(CreateView):
+    #操作するモデルを指示
+    model = Category
+    #成功した時のJUMP先を指示
+    success_url = reverse_lazy("myapp:category_list")
+    template_name = 'category_form.html'
+
+    #フィール設定（ないとエラー）
+    fields = ['is_check', 'title', 'title_num', 'foreign_number']
+    # 投稿に成功した時に実行される処理
+    def get_success_url(self):
+        messages.success(self.request, '記事を投稿しました。')
+        return resolve_url("myapp:category_list")
